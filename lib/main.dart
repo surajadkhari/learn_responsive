84af927efc6b7cc750cac8057ca4a8f9c8b199bb
surajadkhari <surajadkhari@gmail.com>
2022-04-27 11:50:27 +0545

    arrange folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'login_page.dart';
+import 'responsive/media query/login_page.dart';
 
 void main() {
   runApp(const MyApp());
